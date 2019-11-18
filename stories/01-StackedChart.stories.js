9d66ce09e7ed94292b5563b10de855aec5206bb8
Besart Shyti <besartshyti@gmail.com>
2019-11-18 22:50:34 +0000

    [fix] make sure StackedChart stories are up to date

--- stories/01-StackedChart.stories.js
+++ stories/01-StackedChart.stories.js
@@ -7,26 +7,37 @@ export default {title: 'StackedChart'};
 export const toStorybook = () => {
 
   const props = {
+    height: 400,
+    width: 900,
     colors: {
       other: '#006d2c',
       hydro: '#9ecae1',
       solar: '#c7e9c0'
     },
     data: [
-      { other: 20, solar: 30, hydro: 50, date: '2019-11-13T00:00Z' },
-      { other: 30, solar: 30, hydro: 40, date: '2019-11-13T02:00Z' },
-      { other: 50, solar: 25, hydro: 25, date: '2019-11-13T04:00Z' },
-      { other: 60, solar: 30, hydro: 10, date: '2019-11-13T06:00Z' },
-      { other: 40, solar: 30, hydro: 30, date: '2019-11-13T08:00Z' },
-      { other: 30, solar: 40, hydro: 30, date: '2019-11-13T10:00Z' },
-      { other: 10, solar: 70, hydro: 20, date: '2019-11-13T12:00Z' },
-      { other: 30, solar: 40, hydro: 30, date: '2019-11-13T14:00Z' },
-      { other: 30, solar: 10, hydro: 60, date: '2019-11-13T16:00Z' },
-      { other: 20, solar: 50, hydro: 30, date: '2019-11-13T18:00Z' },
-      { other: 30, solar: 30, hydro: 40, date: '2019-11-13T20:00Z' },
+      { other: 20, solar: 30, hydro: 50, date: undefined },
+      { other: 30, solar: 30, hydro: 40, date: undefined },
+      { other: 50, solar: 25, hydro: 25, date: undefined },
+      { other: 60, solar: 30, hydro: 10, date: undefined },
+      { other: 40, solar: 30, hydro: 30, date: undefined },
+      { other: 30, solar: 40, hydro: 30, date: undefined },
+      { other: 10, solar: 70, hydro: 20, date: undefined },
+      { other: 30, solar: 40, hydro: 30, date: undefined },
+      { other: 30, solar: 10, hydro: 60, date: undefined },
+      { other: 20, solar: 50, hydro: 30, date: undefined },
+      { other: 30, solar: 30, hydro: 40, date: undefined },
     ],
     currValueX: value => console.log(`currValueX`, value),
   };
+
+  // make sure data is relevant to today
+  props.data.forEach((dataPoint, idx) => {
+
+    const time = new Date();
+    time.setHours(0, 0, 0, 0);
+    time.setHours(idx * 2);
+    dataPoint.date = time;
+  });
 
   return (
     <StackedChart {...props} />
